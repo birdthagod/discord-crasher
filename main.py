83b7dd7c814fae7116bf407850f76e9dd126d5da
beikommen <56656809+beikommen@users.noreply.github.com>
2021-04-27 19:58:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 import os,sys
 import subprocess
 import glob
+import os
+import sys
 from colorama import Fore, Back, Style
-def clear():
-    os.system("clear")
 
+def clear():
+    os.system("clear;cls")
+    
+def blank():
+    print(" ")
+
+def blocks():
+    print(f"""    {Fore.BLUE }[+++]{Fore.RESET}==================={Fore.BLUE }[+++]{Fore.RESET}\n""")
+    
 def main():
-    print(" ")
-    print(f"""    {Fore.BLUE }[+++]{Fore.RESET}==================={Fore.BLUE }[+++]{Fore.RESET}\n""")
+    blank()
+    blocks()
     firstf = input(f"""    {Fore.BLUE }${Fore.RESET} Video => """)
-    print(f"""    {Fore.BLUE }[+++]{Fore.RESET}==================={Fore.BLUE }[+++]{Fore.RESET}\n""")
+    blocks()
     os.system(f"""ffmpeg -i black.mp4 -pix_fmt yuv444p new-vid.mp4""")
-    print(" ")
-    print(f"""    {Fore.BLUE }[+++]{Fore.RESET}==================={Fore.BLUE }[+++]{Fore.RESET}\n""")
+    blank()
+    blocks()
     open("code.txt", "w").close()
     file = open("code.txt","w") 
     file.write(f"""file {firstf}\n""")
     file.write(f"""file new-vid.mp4""")
     file.close()
-    print(" ")
-    print(f"""    {Fore.BLUE }[+++]{Fore.RESET}==================={Fore.BLUE }[+++]{Fore.RESET}\n""")
+    blank()
+    blocks()
     os.system("ffmpeg -f concat -i code.txt -codec copy crasher.mp4")
     os.system("rm -rf code.txt")
     os.system("rm -rf new-vid.mp4")
